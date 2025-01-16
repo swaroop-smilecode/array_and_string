9348626e51a0efacb69a6078ffbd58448ca3dfb9
Swaroop <swaroopiit@gmail.com>
2025-01-16 11:36:26 +0530

    Added some more content :)

--- p05_uncompress/p05_uncompress.py
+++ p05_uncompress/p05_uncompress.py
@@ -1,18 +1,18 @@
 def uncompress(s):
-    numbers = {1,2,3,4,5,6,7,8,9}
-    result = []
-    i = 0
-    j = 0
+  numbers = '0123456789'
+  result = []
+  i = 0
+  j = 0
+  while j < len(s):
+    if s[j] in numbers:
+      j += 1
+    else:      
+      num = int(s[i:j])
+      result.append(s[j] * num)
+      j += 1
+      i = j
+      
+  return ''.join(result)
 
-    while j < len(s):
-        if s[j] in numbers:
-            J += 1
-        else:
-            num = int(s[i:j])
-            result.append(s[j]*num)
-            j += 1
-            i = j
-    
-    return "".join(result)
 
 print(uncompress("2c3a1t")) # -> 'ccaaat'
